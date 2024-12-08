67e545afc359639ba243ac0fdeec7dc57087e485
mike <michael.keiblinger@gmail.com>
2024-12-07 19:04:19 -0800

    add setup

---
+++ setup.sh
@@ -0,0 +1,6 @@
+sudo apt install -y cmake make gcc g++ wget
+wget https://raw.githubusercontent.com/mikex86/reduce_pipelining_test/refs/heads/master/CMakeLists.txt
+wget https://raw.githubusercontent.com/mikex86/reduce_pipelining_test/refs/heads/master/half_duplex_test.cpp
+wget https://raw.githubusercontent.com/mikex86/reduce_pipelining_test/refs/heads/master/full_duplex_test.cpp
+# wget https://raw.githubusercontent.com/mikex86/reduce_pipelining_test/refs/heads/master/half_duplex_reduce_gcloud.sh
+# wget https://raw.githubusercontent.com/mikex86/reduce_pipelining_test/refs/heads/master/full_duplex_reduce_gcloud.sh
